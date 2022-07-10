fbd34d153fa46a80a85334343f59edb030a85954
Karger <dirk.karger@wsl.ch>
2022-07-10 09:46:41 +0000

    Update classes/BioClim.py

--- classes/BioClim.py
+++ classes/BioClim.py
@@ -62,11 +62,11 @@ class quarter_class:
     
     Creates quarters from monthly climatologies.
 
-    param: target_variable: the variable that is to be aggregated for a quarter_class
-    param: quarter_variable: the variable that forms the basis for the quarters (e.g. precipitation for 'wettest' quarter)
-    param: agg_target: the way how the target_variable is aggregated (e.g. mean)
-    param: agg_quarter: the way how the quarter_variable is aggregated (e.g. max)
-    param: find_fun: determines what needs to be found (e.g. minimum temperature of coldes quarter = 'min')
+    :param target_variable: the variable that is to be aggregated for a quarter_class
+    :param quarter_variable: the variable that forms the basis for the quarters (e.g. precipitation for 'wettest' quarter)
+    :param agg_target: the way how the target_variable is aggregated (e.g. mean)
+    :param agg_quarter: the way how the quarter_variable is aggregated (e.g. max)
+    :param find_fun: determines what needs to be found (e.g. minimum temperature of coldes quarter = 'min')
 
     """
     def __init__(self, target_variable, quarter_variable, agg_target, agg_quarter, find_fun):
@@ -81,11 +81,11 @@ class quarter_class:
         """
         creates quarters from a vector of 12 monthly means
 
-        param: xv: monthly values of a variable
-        param: agg: aggregation method
+        :param xv: monthly values of a variable
+        :param agg: aggregation method
 
-        return: array of size 4 containing the quarters
-        rtype: array
+        :return: array of size 4 containing the quarters
+        :rtype: array
         """
         #xv = [11, 20, 30, 104, 95, 96, 75, 85, 90, 190, 181, 172]
         monthv = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]
@@ -107,6 +107,15 @@ class quarter_class:
         return a1
 
     def _comp_quarters_(self, target_variable, quarter_variable): # ,array1, array2, agg1, agg2, fun2
+        """
+        Compare quarters for a target variable
+
+        :param target_variable: the variable that is to be aggregated for a quarter_class
+        :param quarter_variable: the variable that forms the basis for the quarters (e.g. precipitation for 'wettest' quarter)
+
+        :return: positional location of the comparision (e.g. the position of the coldest quarter in the array)
+        :rtype: integer
+        """
         target_quarter = self._create_quarter_(target_variable, self.agg_target)
         quarter_quarter = self._create_quarter_(quarter_variable, self.agg_quarter)
         if self.find_fun == 'max':
@@ -117,7 +126,12 @@ class quarter_class:
         return a
 
     def comp_quarters_array(self):
-        """Compare quarters"""
+        """
+        Compare quarters over an xarray
+
+        :return: array of target variable for a quarter
+        :rtype: xarray
+        """
         res_arr = xr.apply_ufunc(self._comp_quarters_,  # function to apply
                                  self.target_variable,
                                  self.quarter_variable,# pass arguments.
@@ -130,7 +144,15 @@ class quarter_class:
 
 
 class BioClim:
-    """ climatology class for monthly climatologies """
+    """ 
+    Climatology class to calculate bioclimatic variables from monthly climatologies of temperatures
+    and precipitation. 
+
+    :param tas: Monthly mean daily mean 2m air temperature [Celsius]
+    :param tasmax: Monthly mean daily maximum 2m air temperature [Celsius]
+    :param tasmin: Monthly mean daily minimum 2m air temperature [Celsius]
+    :param pr: Monthly precipitation rate [kg*m**-2*month**-1]
+    """
     def __init__(self, pr, tas, tasmax, tasmin):
         """ Create a set of baseline clims """
         self.tas = tas.load() #chunk({month: -1}) #rename({'tas': 'var'})
@@ -139,40 +161,120 @@ class BioClim:
         self.pr = pr.load() #chunk({month: -1}) #rename({'pr': 'var'})
 
     def _mean_(self, x ):
+        """
+        calculate mean
+
+        :param x: a variable 
+        :return: mean
+        :rtype: float
+        """
         s = np.sum(x)
         n = len(x)
         mean = s/n
         return mean
 
     def _diurnalrange_(self, tasmax, tasmin):
+        """
+        Calculate the mean monthly diurnal range of temperatures
+        
+        :param tasmax: Monthly mean daily maximum 2m air temperature [Celsius]
+        :param tasmin: Monthly mean daily minimum 2m air temperature [Celsius]
+        :return: diurnal range of temperatures [Kelvin]
+        :rtype: float
+        """
         return np.sum(tasmax - tasmin) / 12
 
     def _sd_(self, x):
+        """
+        calculate standard deviation
+
+        :param x: a variable 
+        :return: standard deviation
+        :rtype: float
+        """
         return np.std(x)
 
     def _max_(self, x):
+        """
+        calculate maximum
+
+        :param x: a variable 
+        :return: maximum
+        :rtype: float
+        """
         return np.max(x)
 
     def _min_(self, x):
+        """
+        calculate minimum
+
+        :param x: a variable 
+        :return: minimum
+        :rtype: float
+        """
         return np.min(x)
 
     def _sum_(self, x):
+        """
+        calculate sum
+
+        :param x: a variable 
+        :return: sum
+        :rtype: float
+        """
         return np.sum(x)
 
     def _cv_(self, x):
+        """
+        calculate coefficient or variation
+
+        :param x: a variable 
+        :return: coefficient of variation
+        :rtype: float
+        """
         sigma = self._sd_(x)
         mu = self._mean_(x)
         cv = sigma*100/mu
         return cv
 
+    def _bio3_(self, tasmax, tasmin):
+        """
+        calculate bio3 "Isothermality"
+        The ratio of diurnal variation (bio2) to annual range of air temperature (bio7)
+
+        :param tasmax: Monthly mean daily maximum 2m air temperature [Celsius]
+        :param tasmin: Monthly mean daily minimum 2m air temperature [Celsius]
+        :return: Isothermality [unitless]
+        :rtype: float
+        """
+        bio5 = self._max_(tasmax)
+        bio6 = self._min_(tasmin)
+        bio7 = bio5-bio6
+        bio2 = self._diurnalrange_(tasmax, tasmin)
+        bio3 = bio2 / bio7
+        return bio7
+
     def _bio7_(self, tasmax, tasmin):
+        """
+        calculate bio7 "annual range of air temperature"
+        The difference between the Maximum Temperature of Warmest month and the Minimum Temperature of Coldest month
+
+        :param tasmax: Monthly mean daily maximum 2m air temperature [Celsius]
+        :param tasmin: Monthly mean daily minimum 2m air temperature [Celsius]
+        :return: annual range of air temperature [Kelvin]
+        :rtype: float
+        """
         bio5 = self._max_(tasmax)
         bio6 = self._min_(tasmin)
         bio7 = bio5-bio6
         return bio7
 
     def bio1(self):
-        """Create mean annual temperature"""
+        """
+        Create mean annual temperature
+        :return: mean annual 2m air temperature [Celsius]
+        :rtype: xarray
+        """
         res_arr = xr.apply_ufunc(self._mean_,  # function to apply
                                  self.tas['tas'],  # pass arguments.
                                  input_core_dims=[['month']],
@@ -185,7 +287,11 @@ class BioClim:
         return res_arr
 
     def bio2(self):
-        """Create mean diurnal temperature range"""
+        """
+        Create mean diurnal temperature range
+        :return: mean diurnal temperature range [Kelvin]
+        :rtype: xarray
+        """
         res_arr = xr.apply_ufunc(self._diurnalrange_,
                                  self.tasmax['tasmax'], self.tasmin['tasmin'],
                                  input_core_dims=[['month'], ['month']],
@@ -197,8 +303,12 @@ class BioClim:
         return res_arr
 
     def bio3(self):
-        """ Temperature seasonality"""
-        res_arr = xr.apply_ufunc(self._sd_,
+        """ 
+        Isothermality
+        :return: Isothermality [unitless]
+        :rtype: xarray
+        """
+        res_arr = xr.apply_ufunc(self._bio3_,
                                  self.tas['tas'],
                                  input_core_dims=[['month']],
                                  dask='parallelized',
