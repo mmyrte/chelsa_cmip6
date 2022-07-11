ba78aff67c83b47f1b341c0570ca7f5f199cc096
Karger <dirk.karger@wsl.ch>
2022-07-11 09:08:27 +0000

    Update classes/GetClim.py

--- classes/GetClim.py
+++ classes/GetClim.py
@@ -169,9 +169,7 @@ class chelsaV2:
         """
         a = []
         for month in range(1, 13):
-            url = 'https://envicloud.os.zhdk.cloud.switch.ch/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/' 
-            + self.variable_id + '/CHELSA_' + self.variable_id + '_' + '%02d' % (
-                month,) + '_1981-2010_V.2.1.tif'
+            url = 'https://envicloud.os.zhdk.cloud.switch.ch/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/' + self.variable_id + '/CHELSA_' + self.variable_id + '_' + '%02d' % (month,) + '_1981-2010_V.2.1.tif'
             a.append(url)
 
         ds = self._crop_ds_(xr.concat([xr.open_rasterio(i) for i in a], 'time'))
